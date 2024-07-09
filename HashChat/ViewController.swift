c9b999c0beb39fd4f354b00faa993bfc61d13faa
sh-raz <rahmani.shilan2@gmail.com>
2024-07-09 10:08:23 +0100

    Initial commit, empty project + gitignore file

---
+++ HashChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HashChat
+//
+//  Created by shilani on 09/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
